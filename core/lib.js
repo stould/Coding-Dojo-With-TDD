a29d51d593d654f76b1da4395a9d489ed4e6d3f1
stould <vinirresko@gmail.com>
2019-05-06 02:24:58 -0300

    Add code to parseInput function

--- core/lib.js
+++ core/lib.js
@@ -1,7 +1,37 @@
 //------ lib.js ------
 
 const parseInput = function(inputString) {
+    if(!inputString) {
+        throw new Error('Input string cannot be empty.');
+    }
 
+    const firstCharacter = inputString[0];
+
+    //checks if first character is an operator
+    if(isNaN(firstCharacter)) {
+        inputString = inputString.substring(1);
+    }
+
+    //skips first character
+    const values = inputString.split(/[+-]/).map(function(number) {
+        return parseInt(number);
+    });
+
+    //makes first value as negative
+    if(firstCharacter == '-') {
+        values[0] = -values[0];
+    }
+
+    const operators = inputString.split(/[0-9]/).filter((value) => {
+        return value.length > 0;
+    });
+
+    const parsedInput = {
+        'values': values,
+        'operators': operators,
+    };
+
+    return parsedInput;
 };
 
 const sum = function(num1, num2) {
