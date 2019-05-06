6cff485909d3dfeb890ef74a97e6ea0234cbceac
stould <vinirresko@gmail.com>
2019-05-06 02:21:55 -0300

    Add tests to sum and subtract

--- test/lib.test.js
+++ test/lib.test.js
@@ -8,10 +8,16 @@ describe('Testing lib functions', function() {
     });
 
     describe('Sum function', function() {
-
+        it('sums two numbers', function() {
+            const result = lib.sum(2, 3);
+            expect(result).to.equal(5);
+        });
     });
 
     describe('Subtract function', function() {
-
+        it('Subtract two numbers', function() {
+            const result = lib.subtract(5, 3);
+            expect(result).to.equal(2);
+        });
     });
 });
