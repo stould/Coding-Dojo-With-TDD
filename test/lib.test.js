6c74b954a03ee46db0f524f9320483c94f818646
stould <vinirresko@gmail.com>
2019-05-06 02:23:02 -0300

    Add tests to parseInput function

--- test/lib.test.js
+++ test/lib.test.js
@@ -4,7 +4,60 @@ var lib = require('../core/lib');
 describe('Testing lib functions', function() {
 
     describe('ParseInput function', function() {
-
+        it('parses empty string', function() {
+            const mockedInput = '';
+            expect(() => lib.parseInput(mockedInput)).to.throw(Error, 'Input string cannot be empty.');
+        });
+        it('parses one number from string', function() {
+            const mockedInput = '5';
+            const mockedOutput = {
+                'values': [5],
+                'operators': [],
+            };
+            const result = lib.parseInput(mockedInput);
+            expect(result).to.be.an('Object');
+            expect(result).to.deep.equal(mockedOutput);
+        });
+        it('parses two numbers from string', function() {
+            const mockedInput = '5-2';
+            const mockedOutput = {
+                'values': [5,2],
+                'operators': ['-'],
+            };
+            const result = lib.parseInput(mockedInput);
+            expect(result).to.be.an('Object');
+            expect(result).to.deep.equal(mockedOutput);
+        });
+        it('parses multiple numbers from string', function() {
+            const mockedInput = '5+1-2';
+            const mockedOutput = {
+                'values': [5,1,2],
+                'operators': ['+','-'],
+            };
+            const result = lib.parseInput(mockedInput);
+            expect(result).to.be.an('Object');
+            expect(result).to.deep.equal(mockedOutput);
+        });
+        it('parses even when first character is an operator plus', function() {
+            const mockedInput = '+5+1-2';
+            const mockedOutput = {
+                'values': [5,1,2],
+                'operators': ['+','-'],
+            };
+            const result = lib.parseInput(mockedInput);
+            expect(result).to.be.an('Object');
+            expect(result).to.deep.equal(mockedOutput);
+        });
+        it('parses even when first character is an operator minus', function() {
+            const mockedInput = '-5+1-2';
+            const mockedOutput = {
+                'values': [-5,1,2],
+                'operators': ['+','-'],
+            };
+            const result = lib.parseInput(mockedInput);
+            expect(result).to.be.an('Object');
+            expect(result).to.deep.equal(mockedOutput);
+        });
     });
 
     describe('Sum function', function() {
