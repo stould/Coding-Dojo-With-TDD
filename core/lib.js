a0c61812970a73e03971570026da49d23b3f7f13
stould <vinirresko@gmail.com>
2019-05-06 02:22:27 -0300

    Add code to sum and subtract

--- core/lib.js
+++ core/lib.js
@@ -5,11 +5,11 @@ const parseInput = function(inputString) {
 };
 
 const sum = function(num1, num2) {
-
+    return num1 + num2;
 };
 
 const subtract = function(num1, num2) {
-
+    return num1 - num2;
 };
 
 module.exports = {
